e7dac2f4537be8ebd904c0d5f0629fe579bc2f86
Tharindu1527 <157977719+Tharindu1527@users.noreply.github.com>
2026-03-15 14:23:36 +0530

    Add comments for code clarity in top_n_mapper.py
    Added comments to clarify code functionality.

--- top_n_mapper.py
+++ top_n_mapper.py
@@ -1,6 +1,9 @@
 import sys
+# Loop through each line
 for line in sys.stdin:
     parts = line.strip().split('\t')
+
+    # Check if the line has exactly 3 values
     if len(parts) == 3:
         category, revenue, count = parts
-        print('ALL' + '\t' + str(float(revenue)) + '\t' + category + '\t' + count)
+        print('ALL' + '\t' + str(float(revenue)) + '\t' + category + '\t' + count)   # 'ALL' is used as a global key
